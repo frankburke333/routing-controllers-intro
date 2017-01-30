209426ac558c41f8df8e97f558482155b12281be
frankburke333 <frank.burke@gmail.com>
2017-01-30 13:17:36 -0500

    add @header message for each page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,18 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "This is the Welcome page"
     render :welcome
   end
 
   def about
+    @header = "About how we crush it on the regular"
     render :about
   end
 
+
+  def contest
+    @header = "You wanna contest me?"
+    render :contest
+  end
 end
